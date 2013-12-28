880953806bb78da50c4476211de7afd7aee4ce5e
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2013-12-28 22:49:44 +0600

    Mongodb do not recconecting initially, but does if connected

--- replSet.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /tmp/mongodb/data/rs0
-mkdir -p /tmp/mongodb/data/rs1
-mkdir -p /tmp/mongodb/data/rs2
-mkdir -p /tmp/mongodb/log
-mongod -f test/config/rs0.conf --fork
-mongod -f test/config/rs1.conf --fork
-mongod -f test/config/rs2.conf --fork
