4d0b8db0d2dadbbbf9d0ba751f7a61287407764c
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-08-02 15:05:59 +0500

    Clean up a little bit

--- index.js
+++ index.js
@@ -2,23 +2,20 @@
 
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
-var objectAssign = require('object-assign');
 
 module.exports = function (uri, opts) {
 	if (typeof uri !== 'string') {
 		throw new TypeError('Expected uri to be a string');
 	}
 
-	opts = objectAssign({
-		property: 'db'
-	}, opts);
+	opts = opts || {};
 
 	var connection = MongoClient.connect(uri, opts);
 
 	return function (req, res, next) {
 		connection
 			.then(function (db) {
-				req[opts.property] = db;
+				req[opts.property || 'db'] = db;
 				next();
 			})
 			.catch(next);
